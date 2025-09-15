264b5044f83d1d4b3f55d3f441a656a2c5076e0a
Nathandiv <k.n.hlungwani@gmail.com>
2025-09-15 12:53:50 +0200

    added all routings on app.route.ts and added contact componenet whioch was missing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
+import { FaqComponent } from './pages/faq/faq.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+      { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'testimonials', component: TestimonialsComponent },
+  { path: 'contact', component: ContactComponent},
+  { path: 'faq', component: FaqComponent },
+  { path: '**', redirectTo: '' }
+
+];
