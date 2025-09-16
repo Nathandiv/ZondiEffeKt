ce679b40be58b9ec508ff3644b3136504e74792c
Nathandiv <k.n.hlungwani@gmail.com>
2025-09-16 15:34:02 +0200

    added about and fixed the changes

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { FooterComponent } from "../../components/footer/footer.component";
-import { HeaderComponent } from "../../components/header/header.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-about',
-  imports: [FooterComponent, HeaderComponent],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  skills = [
+    { name: 'Photography', level: 95 },
+    { name: 'Videography', level: 90 },
+    { name: 'Photo Editing', level: 92 },
+    { name: 'Video Editing', level: 88 },
+    { name: 'Drone Photography', level: 85 },
+    { name: 'Creative Direction', level: 93 }
+  ];
 
+  achievements = [
+    {
+      year: '2023',
+      title: 'Wedding Photographer of the Year',
+      organization: 'Regional Photography Awards'
+    },
+    {
+      year: '2022',
+      title: 'Best Corporate Video Campaign',
+      organization: 'Business Media Awards'
+    },
+    {
+      year: '2021',
+      title: 'Rising Star in Photography',
+      organization: 'Creative Arts Society'
+    }
+  ];
 }
