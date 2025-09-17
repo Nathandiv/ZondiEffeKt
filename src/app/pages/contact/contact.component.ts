52747b32c107040ae64bce8d4a51cf8801fa69ce
Nathandiv <k.n.hlungwani@gmail.com>
2025-09-17 16:48:52 +0200

    made changes to contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent  {
+  isQuoteModalOpen = false;
+  isConsentModalOpen = false;
 
+  openModal(modalId: string) {
+    if (modalId === 'quote') {
+      this.isQuoteModalOpen = true;
+    } else if (modalId === 'consent') {
+      this.isConsentModalOpen = true;
+    }
+  }
+
+  closeModal(modalId: string) {
+    if (modalId === 'quote') {
+      this.isQuoteModalOpen = false;
+    } else if (modalId === 'consent') {
+      this.isConsentModalOpen = false;
+    }
+  }
+
+  openConsentModal() {
+    this.isConsentModalOpen = true;
+  }
+
+  closeConsentModal() {
+    this.isConsentModalOpen = false;
+  }
 }
